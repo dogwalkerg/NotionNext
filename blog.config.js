1c9959e5e26a9b566372ad304c9f335a2214442e
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2023-04-26 07:36:41 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,8 +14,8 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '莱特宁亮化工程', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚MRGU', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '顾乡情', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '分享随便，杂文或阅读体验', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.ipone.ga', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'MRGU,莱特宁,Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
