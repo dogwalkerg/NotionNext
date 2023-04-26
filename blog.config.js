fec3268e8f34ab55d2310b89664ea217df208d32
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2023-04-26 14:19:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -144,7 +144,7 @@ const BLOG = {
   WIDGET_PET_SWITCH_THEME: true, // 点击宠物挂件切换博客主题
 
   // 音乐播放插件
-  MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || true, // 是否使用音乐播放插件
+  MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || false, // 是否使用音乐播放插件
   MUSIC_PLAYER_VISIBLE: process.env.NEXT_PUBLIC_MUSIC_PLAYER_VISIBLE || true, // 是否在左下角显示播放和切换，如果使用播放器，打开自动播放再隐藏，就会以类似背景音乐的方式播放，无法取消和暂停
   MUSIC_PLAYER_AUTO_PLAY:
     process.env.NEXT_PUBLIC_MUSIC_PLAYER_AUTO_PLAY || true, // 是否自动播放，不过自动播放时常不生效（移动设备不支持自动播放）
@@ -156,16 +156,17 @@ const BLOG = {
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
-      name: 'penguinmusic',
+      name: '风を共に舞う気持ち',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://cdn.pixabay.com/download/audio/2021/12/22/audio_9da2a60074.mp3',
+      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
       cover:
         'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
     },
     {
-      name: 'My Universe',
+      name: '王都グランセル',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://cdn.pixabay.com/download/audio/2023/04/21/audio_8579ca297f.mp3',
+      url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
+      cover:
         'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
     }
   ],
